892bf8fe38fcbda42a4ceb744638f470888300ef
Samuel Oliveira <samuelloliiveira42@gmail.com>
2020-08-09 00:36:37 -0300

    Ultima aula. Refatorando código, arrumando toda parte de consulta e cadastro no banco. Aqui acaba a NLW#2

--- src/database/test.js
+++ src/database/test.js
@@ -10,7 +10,7 @@ Database.then(async (db) => {
     }
 
     classValue = {
-        subject: "Química",
+        subject: "1",
         cost: "20",
         //O proffy id virá pelo banco de dados
     }
@@ -31,5 +31,30 @@ Database.then(async (db) => {
 
     //await createProffy(db, {proffyValue, classValue, classScheduleValues})
 
-    //Consultar dados
-})
+    //Consultar dados inseridos no banco
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    //console.log(selectedProffys)
+
+    //consultar as classes de um determinado proffy
+    //e trazer junto os dados do proffy
+    const selectClassesAndProffys= await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+    //console.log(selectClassesAndProffys)
+
+    //O horário do time_from precisa ser menor ou igual ao horário solicitado
+    //O time_to precisa está acima
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "1300"
+        AND class_schedule.time_to > "1300"
+    `)
+
+    console.log(selectClassesSchedules)
+}) 
